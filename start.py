fd0daabea778af30c1d6a3e3d1ff1a9387046786
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-05 00:47:23 +0530

    adding quotes

---
+++ start.py
@@ -0,0 +1,17 @@
+#Radhe Radhe Hare Krishn
+import random
+
+choice='y'
+while(choice=='y'):
+    quote=["Love is the bridge between you and everything.",
+           "The more love you give, the more your spirit glows.",
+           "Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.",
+           "When the soul lies down in that grass, the world is too full to talk about. Ideas, language, even the phrase each other doesn't make any sense.",
+           "To love another person is to see the face of God.",
+           "Love and compassion are necessities, not luxuries. Without them, humanity cannot survive.",
+           "True love is born from understanding.",
+           "Love is the purest form of a soul at peace.",
+           "The spiritual journey is the unlearning of fear and the acceptance of love.",
+           "Let yourself be silently drawn by the strange pull of what you really love. It will not lead you astray."
+           ]
+        
